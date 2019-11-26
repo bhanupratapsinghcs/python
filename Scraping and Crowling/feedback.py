b1adb041fabc9fd094fbc15388e86ff730fa8624
Bhanu pratap singh <pbhanu314@gmail.com>
2019-11-26 21:34:57 +0530

    Feedback file update

--- Scraping and Crowling/feedback.py
+++ Scraping and Crowling/feedback.py
@@ -4,8 +4,6 @@ import time
 username = "181500191"
 password = "ELAW"
 
-sub=""
-
 driver = webdriver.Chrome("C:/Users/Bhanu/Desktop/chromedriver_win32/chromedriver.exe")
 
 driver.get("http://glauniversity.in:8083/StudentHome")
@@ -25,6 +23,8 @@ driver.find_element_by_id("btn_login").click()
 time.sleep(3)
 
 elems = driver.find_elements_by_xpath('//*[@id="Ul1"]/li[10]/ul/li[2]/a')
-sub = [elem.get_attribute('href') for elem in elems]
+sub = ""
+for elem in elems:
+    sub = elem.get_attribute("href")
 
 driver.get(sub)
